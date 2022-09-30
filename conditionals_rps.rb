44d4ae8c9ab4555d19fee047f0aed450ae33fcd6
lett39 <lettaw30@gmail.com>
2022-09-30 20:34:48 +0000

    conditionals update

--- conditionals_rps.rb
+++ conditionals_rps.rb
@@ -6,4 +6,36 @@
 # Based on what the player chose, prints "You won!" or "You lost!" or "You tied!"
 # If you need a refresher on the rules of Rock, Paper, Scissors: https://en.wikipedia.org/wiki/Rock%E2%80%93paper%E2%80%93scissors
 
+
 p "Please choose rock, paper, or scissors:"
+# the_game=gets.chomp
+
+the_game = "paper"
+
+p the_game
+
+computer_pick=["rock", "paper", "scissors"].sample
+
+p "The computer played " + computer_pick
+
+if the_game=="rock"
+  p "You played rock" 
+
+  if computer_pick == "rock"
+    p "tie"
+  elsif computer_pick == "paper"
+  
+  elsif
+  end
+
+elsif the_game == "paper"
+  p "You played paper"
+elsif the_game == "scissors" 
+  p "You played scissors"
+end  
+
+p "You lost!"
+
+p "You won!"
+
+p "You tied!"
